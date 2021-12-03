b4ce49bb4d3fe25d01cd1b7813ce5498255bcbe1
jiyounghan <jiyounghaan@gmail.com>
2021-12-03 14:28:29 +0900

    chore: 게시판 글 조회 dto 추가
    - 게시판 수정 커밋에 dto 빠뜨려서 추가함
    
    #65

---
+++ web/src/main/java/com/tripper/dto/response/board/GetBoardDto.java
@@ -0,0 +1,39 @@
+package com.tripper.dto.response.board;
+
+import com.tripper.domain.board.Board;
+import com.tripper.domain.board.BoardStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter @Setter
+@AllArgsConstructor
+public class GetBoardDto {
+    private Long id;
+    private String title;
+    private String destination;
+    private String startDate;
+    private String endDate;
+    private int recruitment;
+    private String content;
+    private BoardStatus status;
+    private int hits;
+    private int likes;
+    private LocalDateTime dateTime;
+
+    public GetBoardDto(Board board) {
+        this.id = board.getId();
+        this.title = board.getTitle();
+        this.destination = board.getDestination();
+        this.startDate = board.getStartDate();
+        this.endDate = board.getEndDate();
+        this.recruitment = board.getRecruitment();
+        this.content = board.getContent();
+        this.status = board.getStatus();
+        this.hits = board.getHits();
+        this.likes = board.getLikes();
+        this.dateTime = board.getDateTime();
+    }
+}
