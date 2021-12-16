c77cf07c72dec1ab082e9e4e61d0da22bdf327ce
jiyounghan <jiyounghaan@gmail.com>
2021-12-16 15:26:25 +0900

    Refactor: 회원 Entity 수정
    - 기존에 생성 메서드의 매개변수를 dto로 묶어서 넘겨줬던 것을 풀어써서 넘어오는 값이 무엇인지 직관적으로 알 수 있도록 함
    - 비즈니스 로직 추가(정보 및 권한 수정, 이메일 인증 코드 저장)
    
    #92

--- web/src/main/java/com/tripper/domain/user/User.java
+++ web/src/main/java/com/tripper/domain/user/User.java
@@ -5,9 +5,8 @@ import com.tripper.domain.BaseTimeEntity;
 import com.tripper.domain.board.Board;
 import com.tripper.domain.hotel.Hotel;
 import com.tripper.domain.hotel.Reservation;
-import com.tripper.dto.request.user.CreateUserDto;
-import com.tripper.dto.request.user.UpdateUserDto;
 import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,43 +15,34 @@ import javax.persistence.*;
 import java.util.*;
 
 import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
 
 /**
  * @author HanJiyoung
  * 회원 정보 엔티티 클래스
  */
 @Entity
-@Getter @Setter
-@NoArgsConstructor
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+@Slf4j
 public class User extends BaseTimeEntity implements UserDetails {
 
     @Id @GeneratedValue(strategy = IDENTITY)
     @Column(name = "user_id")
-    private Long id; // PK
+    private Long id;
 
     @Column(unique = true, nullable = false)
     private String memId;
 
-//    @Column(nullable = false)
     private String password;
-
-//    @Column(nullable = false)
     private String name;
-
-//    @Column(nullable = false)
     private String phone;
-
-//    @Column(nullable = false)
     private String email;
-
-//    @Column(nullable = false)
     private String nickname;
 
-//    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private Role auth;
 
-//    @Column
     private String emailAuthCode;
 
     @JsonIgnore
@@ -65,16 +55,16 @@ public class User extends BaseTimeEntity implements UserDetails {
 
     @JsonIgnore
     @OneToMany(mappedBy = "user")
-    private List<Reservation> resrvations = new ArrayList<>();
+    private List<Reservation> reservations = new ArrayList<>();
 
     @Builder
-    public User(CreateUserDto createUserDto, Role auth) {
-        this.memId = createUserDto.getMemId();
-        this.password = createUserDto.getPassword();
-        this.name = createUserDto.getName();
-        this.phone = createUserDto.getPhone();
-        this.email = createUserDto.getEmail();
-        this.nickname = createUserDto.getEmail();
+    public User(String memId, String password, String name, String phone, String email, String nickname, Role auth) {
+        this.memId = memId;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.nickname = nickname;
         this.auth = auth;
     }
 
@@ -120,11 +110,19 @@ public class User extends BaseTimeEntity implements UserDetails {
     }
 
     /* 회원정보 수정 */
-    public void updateUser(UpdateUserDto updateUserDto) {
-        this.password = updateUserDto.getPassword();
-        this.phone = updateUserDto.getPhone();
-        this.email = updateUserDto.getEmail();
-        this.nickname = updateUserDto.getNickname();
-        this.auth = updateUserDto.getAuth();
+    public void updateUserInfo(String password, String phone) {
+        this.password = password;
+        this.phone = phone;
     }
+
+    /* 권한 수정*/
+    public void updateAuth(Role auth) {
+        this.auth = auth;
+    }
+
+    /* 이메일 인증 코드 저장 */
+    public void setEmailAuthCode(String emailAuthCode) {
+        this.emailAuthCode = emailAuthCode;
+    }
+
 }
