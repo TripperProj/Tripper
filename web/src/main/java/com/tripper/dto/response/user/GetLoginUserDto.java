197c84696292da9e1ca5d9235f8aa0bff85cc8c2
jiyounghan <jiyounghaan@gmail.com>
2021-12-16 15:21:25 +0900

    Refactor: 회원 관련 DTO 수정
    - 리스트 dto에 대해 total 값을 추가함
    - 로그인 후 반환되는 dto의 클래스명을 좀더 직관적으로 수정, 사용자 권한도 반환하도록 추가
    - 회원 정보 수정 시 이메일, 닉네임, 권한은 수정할 수 없도록 삭제
    
    #92

--- web/src/main/java/com/tripper/dto/response/user/GetLoginUserDto.java
+++ web/src/main/java/com/tripper/dto/response/user/GetLoginUserDto.java
@@ -1,19 +1,20 @@
 package com.tripper.dto.response.user;
 
+import com.tripper.domain.user.Role;
 import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.io.Serializable;
-
-@ApiModel(value = "토큰 Response DTO")
+@ApiModel(value = "로그인 Response DTO")
 @Getter
 @AllArgsConstructor
-public class GetJwtDto implements Serializable {
+public class GetLoginUserDto {
 
     private static final long serialVersionUID = -8091879091924046844L;
     private final String jwttoken;
+    private Long user_id;
     private String memId;
+    private Role auth;
 
     public String getToken() {
         return this.jwttoken;
