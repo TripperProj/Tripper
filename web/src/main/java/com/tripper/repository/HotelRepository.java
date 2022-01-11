99b55fd5b22f74dddadd773f08bb5392302ad0af
jiy0ung <jiyounghaan@gmail.com>
2022-01-11 19:16:35 +0900

    Chore: 호텔 매니저가 등록한 호텔들만 조회하는 기능 추가
    #135

--- web/src/main/java/com/tripper/repository/HotelRepository.java
+++ web/src/main/java/com/tripper/repository/HotelRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
 
     @Query("SELECT h FROM Hotel h WHERE h.id = :id")
     Hotel findByHotelId(@Param("id") Long id);
+
+    List<Hotel> findHotelByUser_MemId(String memId);
 
 }
